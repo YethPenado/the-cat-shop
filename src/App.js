082d8970e496808e9a95ef0edbb04ca39eb21a80
YethPenado <yethpenado@gmail.com>
2019-11-18 20:07:00 -0600

    Product card component created and added more data

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import logo from './icons/logo.png';
+import ProductsList from './components/Cards';
 import './App.css';
 
 function App() {
@@ -8,6 +9,10 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
       </header>
+      <div className="products-container">
+      <h2 className="our-products__title">Our Products</h2>
+        <ProductsList/>
+      </div>
     </div>
   );
 }
---
+++ src/components/Cards.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import ProductsData from '../products/products.json';
+const data = ProductsData.products;
+
+function ProductsList() {
+  return (
+    <ul className="cards-container">
+      {data.map((products)=>{
+      return <li className="card__item">
+        <img className="product-img" src={products.image} alt={products.title} /> 
+        <h3 className="product-title">{products.title}</h3>
+        <p className="product-price">${products.price}</p>
+      </li>
+      })}
+    </ul>
+  );
+}
+
+export default ProductsList;
