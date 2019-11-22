43cef1537406f2b09182b7c2aed46950e6a6408c
Yethsabel Penado Saavedra <y.penado@b7-08.local>
2019-11-21 19:37:29 -0600

    Empezando la funcionalidad Modal

--- src/components/Cards.js
+++ src/components/Cards.js
@@ -10,6 +10,7 @@ function ProductsList() {
         <img className="product-img" src={products.image} alt={products.title} /> 
         <h3 className="product-title">{products.title}</h3>
         <p className="product-price">${products.price}</p>
+        <button onClick="" className="product-details">See more details!</button>
       </li>
       })}
     </ul>
